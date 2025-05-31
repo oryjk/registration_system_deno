5eb2273c72075fa4f2433ac33186266395651f0c
WangRui <wangrui@betalpha.com>
2025-05-31 08:37:34 +0800

    支持用户订单计算，支持用户罚款计算

--- order/user_order_use_case.ts
+++ order/user_order_use_case.ts
@@ -1,10 +1,17 @@
 import { db } from "../db/mysql.ts";
 import { ActivityService } from "../service/activity_service.ts";
+import { UserService } from "../service/user_service.ts";
 import { Activity, ActivityOrder, UserActivityStand } from "../types/activity.ts";
 import { UserInfo } from "../types/user.ts";
+import { groupByYearMonth } from "../utils/date_utils.ts";
 import log from "../utils/logger.ts";
 
 // 定义返回结果的类型
+
+enum BillingType {
+    NormalSeduction = 1,
+    Fine = 2
+}
 interface UserActivityBilling {
     activity_id: string;
     activity_time: Date; // 假设活动时间字段名为 activity_time
@@ -12,11 +19,92 @@ interface UserActivityBilling {
         user_id: number;
         fee: number;
         status: number; // 假设参赛状态字段名为 status
+        billing_type: BillingType; // 假设结算时间字段名为 billing_time
     }[];
 }
 
 export class UserOrderUseCase {
-    constructor(private activityService: ActivityService) {
+    constructor(private activityService: ActivityService, private userService: UserService) {
+    }
+
+    async calculateAllActivitiesBilling(startDate: Date): Promise<Map<string, {
+        user_id: number;
+        fee: number;
+        status: number;
+        billing_type: BillingType;
+    }[]>> {
+        const userInfos = await this.userService.getAllUsersInfo()
+        const allActivities = await this.activityService.getActivities()
+        const allDate = allActivities.map((activity) => {
+            return {
+                date: new Date(activity.holding_date),
+                id: activity.id!
+            };
+        })
+        const yearMonth = groupByYearMonth(allDate);
+
+        const allBillings = allActivities.filter((activity) => {
+            return activity.holding_date > startDate;
+        }).map(async (activity) => {
+            const activityBilling = await this.calculateActivityBilling(activity, userInfos.data);
+            return { id: activityBilling?.activity_id, billing: activityBilling };
+        });
+
+        const billingMap = await Promise.all(allBillings).then((billings) => {
+            return billings.reduce((acc, billing) => {
+                if (billing) {
+                    acc.set(billing.id!, billing.billing!);
+                }
+                return acc;
+            }, new Map<string, UserActivityBilling>)
+        });
+
+        const monthFines = yearMonth.reduce((acc, month) => {
+            const userNotParticipate = month.dates.reduce((acc, date) => {
+                const id = date.id;
+                billingMap.get(id)?.user_billing.filter((userBilling) => {
+                    return userBilling.status != 1;
+                }).forEach((userBilling) => {
+                    if (acc.has(userBilling.user_id)) {
+                        acc.set(userBilling.user_id, acc.get(userBilling.user_id)! + 1);
+                    } else {
+                        acc.set(userBilling.user_id, 1);
+                    }
+                });
+                return acc;
+            }, new Map<number, number>);
+
+            acc.set(month.month, []);
+            userNotParticipate.forEach((value, key) => {
+                if (value > 2) {
+                    acc.get(month.month)?.push({
+                        user_id: key,
+                        fee: 100,
+                        status: 4,
+                        billing_type: 2,
+                    });
+                    return;
+                }
+                if (value > 1) {
+                    acc.get(month.month)?.push({
+                        user_id: key,
+                        fee: 50,
+                        status: 4,
+                        billing_type: 2,
+                    });
+                    return;
+                }
+            });
+            return acc;
+        }, new Map<string, {
+            user_id: number;
+            fee: number;
+            status: number;
+            billing_type: BillingType;
+        }[]>);
+
+        return monthFines
+
     }
 
     async calculateActivityBilling(activity: Activity, userInfos: UserInfo[]): Promise<UserActivityBilling | null> {
@@ -33,20 +121,22 @@ export class UserOrderUseCase {
         const needCalculateUsers = userInfos.filter((user) => {
             return user.create_time < activity.holding_date;
         })
+        
         if (activity.status === 3) {
-            console.log("当前比赛取消或者未成型，算作队员都是出勤状态");
+            console.log(`当前比赛${activity.name} ${new Date(activity.holding_date).toLocaleString()} 取消或者未成型，算作队员都是出勤状态`);
             const userBilling = (needCalculateUsers).map((user) => {
                 return {
                     user_id: user.id,
                     fee: 0,
                     status: 1,
+                    billing_type: 1,
                 };
 
             })
             return {
                 activity_id: activity.id!,
                 activity_time: activity.holding_date,
-                user_billing: userBilling,
+                user_billing: userBilling
             };
         }
         // 查询比赛订单信息，获取费用和参赛状态
@@ -71,18 +161,21 @@ export class UserOrderUseCase {
                             user_id: userStand!.user_id,
                             fee: activityOrder.fee,
                             status: userStand!.stand,
+                            billing_type: 1,
                         };
                     case 2:
                         return {
                             user_id: userStand!.user_id,
                             fee: 0,
                             status: userStand!.stand,
+                            billing_type: 1,
                         };
                     case 3:
                         return {
                             user_id: userStand!.user_id,
                             fee: 0,
                             status: userStand!.stand,
+                            billing_type: 1,
                         };
                     default:
                         console.log(`未知的参赛状态：${userStand!.stand}`);
@@ -90,6 +183,7 @@ export class UserOrderUseCase {
                             user_id: userStand!.user_id,
                             fee: 0,
                             status: 3,
+                            billing_type: 1,
                         };
                 }
 
@@ -98,6 +192,7 @@ export class UserOrderUseCase {
                     user_id: user.id,
                     fee: 0,
                     status: 3,
+                    billing_type: 1,
                 };
             }
         })
--- order/user_order_use_case_test.ts
+++ order/user_order_use_case_test.ts
@@ -1,14 +1,36 @@
 import { ActivityService } from "../service/activity_service.ts";
 import { UserService } from "../service/user_service.ts";
+import { UserInfo } from "../types/user.ts";
 import { UserOrderUseCase } from "./user_order_use_case.ts";
 
 Deno.test("query user performance", async (t) => {
     const acticityService = new ActivityService()
     const activity = await acticityService.getActivityById("e3766899-ecc0-48a7-aa0e-dfba7de31be5")
+    // const activity = await acticityService.getActivityById("2736ab53-18b3-4ad0-98f6-b6e45c988306")
 
     const userService = new UserService()
     const userInfo = await userService.getAllUsersInfo()
-    const userOrderUseCase = new UserOrderUseCase(acticityService);
+    const userOrderUseCase = new UserOrderUseCase(acticityService, userService);
     const billing = await userOrderUseCase.calculateActivityBilling(activity, userInfo.data)
+    // console.log(JSON.stringify(billing))
     console.log(billing)
+    console.log(billing?.user_billing.length)
+});
+
+Deno.test("async test", async () => {
+    const activityService = new ActivityService()
+    const userService = new UserService()
+    const userOrderUseCase = new UserOrderUseCase(activityService, userService);
+    const userInfos = await userService.getAllUsersInfo()
+    const userInfoObject = userInfos.data.reduce((acc, item) => {
+        acc[item.id] = item;
+        return acc;
+    }, {} as Record<number, UserInfo>);
+    const billing = await userOrderUseCase.calculateAllActivitiesBilling(new Date("2024-01-01"))
+    billing.forEach((value, key) => {
+        console.log(key+"-------------")
+        value.forEach((item) => {
+            console.log(`${userInfoObject[item.user_id].real_name}->${item.fee}`)
+        })
+    })
 });
